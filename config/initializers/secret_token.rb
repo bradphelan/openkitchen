631a41ae4f80ba4227c9a416ef2de088a8c41fdb
Brad Phelan <bradphelan@xtargets.com>
2012-01-04 20:35:48 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gobbq::Application.config.secret_token = 'e1a2b150a1dc83e7db280c68442e0d12e91075e0dd99997189798774e9922c7e16e41fbb3935ed99b3e1143f2796a3d32a5d1493eca0c1ed00e61d1a6895ef75'
