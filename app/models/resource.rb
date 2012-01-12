40a411203384602e62d0da68eb25855cdb3cb124
Brad Phelan <bradphelan@xtargets.com>
2012-01-13 00:23:16 +0100

    Get rid of caching. Always buggy

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -11,17 +11,15 @@ class Resource < ActiveRecord::Base
   end
 
   def quantity_produced
-    @quantity_produced ||= resource_producers.inject(0) do |s, v|
+    resource_producers.inject(0) do |s, v|
       v.quantity + s
     end || 0
   end
 
   def quantity_produced_by invitation
-    @quantity_produced_by ||= []
-    @quantity_produced_by[invitation.id] ||=
-      resource_producers.where{invitation_id==my{invitation.id}}.inject(0) do |s, v|
-        v.quantity + s
-      end || 0
+    resource_producers.where{invitation_id==my{invitation.id}}.inject(0) do |s, v|
+      v.quantity + s
+    end || 0
   end
 
 end
