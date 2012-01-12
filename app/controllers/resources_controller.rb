303136dd415f25bfef8a15e2113e08861a1e4514
Brad Phelan <bradphelan@xtargets.com>
2012-01-13 00:22:35 +0100

    Parse the name for quantity

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,21 @@
 class ResourcesController < ApplicationController
   def create
-    @resource = Resource.create! params[:resource]
+
+    new_resource = params[:new_resource]
+    if new_resource[:suggest_something]
+      @resource = Resource.new :event_id => params[:new_resource][:event_id]
+      match = new_resource[:suggest_something].match(/(\d+)\s*(.*)/)
+      if match
+        @resource.quantity = match[1].to_i
+        @resource.name = match[2]
+      else
+        @resource.quantity = 1
+        @resource.name = new_resource[:suggest_something]
+      end
+      @resource.save!
+    end
+
+
     redirect_to :back
   end
 
