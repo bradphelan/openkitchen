844386a7e89b61b1631f9d210eb838bf04e32444
Brad Phelan <bradphelan@xtargets.com>
2012-01-17 00:14:44 +0100

    Set delivery method for mail

--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,6 @@ require File.expand_path('../application', __FILE__)
 Gobbq::Application.initialize!
 
 Gobbq::Application.config.action_mailer.smtp_settings = YAML.load(ERB.new(File.read("#{Rails.root}/config/mail.yml")).result)[Rails.env]
+Gobbq::Application.config.action_mailer.delivery_method = :smtp
+
     
