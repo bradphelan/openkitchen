7889d41eb1507ff4158b4a65e01e67e2aeda7fbb
Brad Phelan <bradphelan@xtargets.com>
2012-01-17 00:31:52 +0100

    Get sendgrid working

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,16 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Gobbq::Application.initialize!
 
-Gobbq::Application.config.action_mailer.smtp_settings = YAML.load(ERB.new(File.read("#{Rails.root}/config/mail.yml")).result)[Rails.env]
-Gobbq::Application.config.action_mailer.delivery_method = :smtp
 
+if Rails.env.production? || Rails.env.development?
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
+end
     
