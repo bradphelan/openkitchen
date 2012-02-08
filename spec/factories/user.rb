8ba0ff1586a9795dd5e3119869f40bacf435e853
Brad Phelan <bradphelan@xtargets.com>
2012-02-08 11:28:25 +0100

    Add registered user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :user do
     email { Forgery::Internet.email_address }
-    password Forgery::Basic.password
+    password "xxxxxx"
+
+    factory :registered_user do
+      registration_completed_at { Time.zone.now }
+    end
   end
 end
