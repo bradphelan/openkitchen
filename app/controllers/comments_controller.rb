59b5ce56c62ee9f1cfe74ea986b5e812c19ccd67
Brad Phelan <bradphelan@xtargets.com>
2012-02-13 22:17:45 +0100

    Use nicer javascript and UJS callbacks

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,33 @@
 class CommentsController < InheritedResources::Base
-  include InheritedResources::DSL
   
-  respond_to :html, :js
+  respond_to :json
 
   defaults :resource_class => Comment, 
-
     :collection_name => 'comment_threads',
     :instance_name => 'comment'
 
   belongs_to :event
-  actions :create
+  actions :create, :destroy
 
 
   def create
+    @event = parent
     @comment = Comment.build_from parent, 
       current_user.id, 
       params[:comment][:body]
     @comment.save
 
-    create! do |success, failure|
-      failure.html { redirect_to edit_event_path(parent) }
-      success.html { redirect_to edit_event_path(parent) }
+    create! do |format|
+      html = render_to_string :partial => "events/comment", :locals => { :event => parent, :comment => @comment }
+      format.js { render :js => html, :content_type => "html" }
+    end
+
+  end
+
+  def destroy
+    # Why is this necessary
+    destroy! do |format|
+      format.json { render :json => @comment }
     end
   end
 
