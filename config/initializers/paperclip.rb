7f599bf10fe218fd5c76763ec33e9216405c2a99
Brad Phelan <bradphelan@xtargets.com>
2012-02-19 14:46:16 +0100

    Remove dump of paperclip options to log

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,6 +2,3 @@
 yml = File.join Rails.root, "config", "paperclip.yml"
 options = YAML.load(ERB.new(File.read(yml)).result)[Rails.env]
 Paperclip::Attachment.default_options.update options.symbolize_keys!
-
-require 'pp'
-pp Paperclip::Attachment.default_options
