079253b72f7fc796e889392c1fc70c038e0ac8d9
Brad Phelan <bradphelan@xtargets.com>
2012-03-05 22:09:36 +0100

    Change omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,9 @@
 if Rails.env.production?
-  OmniAuth.config.full_host = "http://openkitchen.at"
-elsif Rails.env.test?
+  OmniAuth.config.full_host = "http://production.openkitchen.at"
+elsif Rails.env.staging?
   OmniAuth.config.full_host = "http://staging.openkitchen.at"
+elsif Rails.env.test?
+  OmniAuth.config.full_host = "http://localhost.openkitchen.at"
 elsif Rails.env.development?
   OmniAuth.config.full_host = "http://localhost.openkitchen.at"
 end
