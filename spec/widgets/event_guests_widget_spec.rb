1c2dbded1d791b00ef0ace49a5bc7a73bd69b831
Brad Phelan <bradphelan@xtargets.com>
2012-03-07 13:53:08 +0100

    Remove dummy specs

--- spec/widgets/event_guests_widget_spec.rb
+++ spec/widgets/event_guests_widget_spec.rb
@@ -1,16 +1,5 @@
 require 'spec_helper'
 
 describe EventGuestsWidget do
-  has_widgets do |root|
-    root << widget('event_guests')
-  end
-  
-  it "should render :display" do
-    render_widget('event_guest', :display).should have_selector("h1")
-  end
-  
-  it "should render :write" do
-    render_widget('event_guest', :write).should have_selector("h1")
-  end
   
 end
