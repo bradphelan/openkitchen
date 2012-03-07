545ba6a212daaffa0d547df5c24995a0781c9317
Brad Phelan <bradphelan@xtargets.com>
2012-03-07 21:54:45 +0100

    Add girl_friday initialization to unicorn after_fork

--- config/initializers/girl_friday.rb
+++ config/initializers/girl_friday.rb
@@ -1,12 +1,1 @@
-
-COMMENT_QUEUE = GirlFriday::WorkQueue.new(:comment_queue, :size => 3) do |info|
-  CommentMailer.mail_subscribers! info[:id]
-end
-
-DEVISE_QUEUE = GirlFriday::WorkQueue.new(:devise_queue, :size => 3) do |info|
-  DeviseBackgroundMailer.complete_action(info).deliver
-end
-
-INVITE_QUEUE = GirlFriday::WorkQueue.new(:invite_queue, :size => 3) do |info|
-  InviteMailer.invite_email(info[:id]).deliver
-end
+# See unicorn config
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -31,4 +31,18 @@ after_fork do |server, worker|
     Resque.redis = ENV['REDIS_URI']
     Rails.logger.info('Connected to Redis')
   end
+
+
+  # Girl Friday Config
+  ::COMMENT_QUEUE = GirlFriday::WorkQueue.new(:comment_queue, :size => 3) do |info|
+    CommentMailer.mail_subscribers! info[:id]
+  end
+
+  ::DEVISE_QUEUE = GirlFriday::WorkQueue.new(:devise_queue, :size => 3) do |info|
+    DeviseBackgroundMailer.complete_action(info).deliver
+  end
+
+  ::INVITE_QUEUE = GirlFriday::WorkQueue.new(:invite_queue, :size => 3) do |info|
+    InviteMailer.invite_email(info[:id]).deliver
+  end
 end
