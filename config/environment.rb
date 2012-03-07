d38bd00edfe108d64d28311b81fc28b6dfadce15
Brad Phelan <bradphelan@xtargets.com>
2012-03-07 11:03:10 +0100

    Fix sendgrid setup for staging

--- config/environment.rb
+++ config/environment.rb
@@ -3,17 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 OpenKitchen::Application.initialize!
-
-
-if Rails.env.production?
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
-  }
-  ActionMailer::Base.delivery_method = :smtp
-end
-    
