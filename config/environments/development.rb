4023c617d7385121e6e8a2de6961ebf4c0c51053
Brad Phelan <bradphelan@xtargets.com>
2012-03-17 00:34:51 +0100

    Don't set default flag value

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,6 @@ OpenKitchen::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
-  config.assets.compile = true
 
   # Expands the lines which load the assets
   config.assets.debug = true
